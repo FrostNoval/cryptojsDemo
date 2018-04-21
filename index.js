520fd33edaf3369911e97b8077b001ebd88297a3
Amumuer <15232755374@163.com>
2018-04-21 12:25:27 +0800

    18.04.21

---
+++ index.js
@@ -0,0 +1,54 @@
+var crypto = require('crypto');
+
+var aesutil = module.exports = {};
+
+/**
+ * aes加密
+ * @param data 待加密内容
+ * @param key 必须为32位私钥
+ * @returns {string}
+ */
+aesutil.encryption = function (data, key, iv) {
+    iv = iv || "";
+    var clearEncoding = 'utf8';
+    var cipherEncoding = 'base64';
+    var cipherChunks = [];
+    var cipher = crypto.createCipheriv('aes-256-cbc', key, iv);
+    cipher.setAutoPadding(true);
+    cipherChunks.push(cipher.update(data, clearEncoding, cipherEncoding));
+    cipherChunks.push(cipher.final(cipherEncoding));
+    return cipherChunks.join('');
+}
+
+/**
+ * aes解密
+ * @param data 待解密内容
+ * @param key 必须为32位私钥
+ * @returns {string}
+ */
+aesutil.decryption = function (data, key, iv) {
+    if (!data) {
+        return "";
+    }
+    iv = iv || "";
+    var clearEncoding = 'utf8';
+    var cipherEncoding = 'base64';
+    var cipherChunks = [];
+    var decipher = crypto.createDecipheriv('aes-256-cbc', key, iv);
+    decipher.setAutoPadding(true);
+    cipherChunks.push(decipher.update(data, cipherEncoding, clearEncoding));
+    cipherChunks.push(decipher.final(clearEncoding));
+    return cipherChunks.join('');
+}
+//待加密密文，为utf 8 字符串
+var message='helloWorld'
+//随机生成长度为32的16进制字符串。IV称为初始向量，不同的IV加密后的字符串是不同的，加密和解密需要相同的IV。
+var iv = '3214567898765432'
+//秘钥。长度32的16进制字符串。
+var key = "0321ebeba1f75de2d3cd3471af7418a4";
+//进行加密
+var encText=aesutil.encryption(message, key, iv)
+console.log(encText);//8Gp3QX1riZUR8t2rSxY2Ag==
+//进行解密
+var decText=aesutil.decryption(encText,key,iv)
+console.log(decText);//helloWorld
